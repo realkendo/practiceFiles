2e10ddc5268b9e5268066fc433b8a9919afd72ac
realkendo <kennethistifanus1@gmail.com>
2024-10-11 17:04:35 +0100

    new push

---
+++ index.js
@@ -0,0 +1,17 @@
+let name = "Kenneth";
+let age = 4;
+let isOnline = false;
+
+if(age > 17){
+    // action 
+    console.log("Adult ");
+}else{
+    console.log("Under age ");
+}
+
+// types of operators
+// 1. arithmetic operatosr = +,-, /, *, %
+// 2. boolean (comparison) - ==,
+// 3. assignment; =
+// 4. logical operators = &&, || 
+
