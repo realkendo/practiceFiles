6654dbc803897d6b0e93da66235a0f0aca132902
realkendo <kennethistifanus1@gmail.com>
2024-11-21 13:47:57 +0100

    cleaning

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // let isOnline = false;
 
 // if(age > 17){
-//     // action 
+//     // action
 //     console.log("Adult ");
 // }else{
 //     console.log("Under age ");
@@ -13,39 +13,32 @@
 // 1. arithmetic operatosr = +,-, /, *, %
 // 2. boolean (comparison) - ==,
 // 3. assignment; =
-// 4. logical operators = &&, || 
-
+// 4. logical operators = &&, ||
 
 // const body = document.querySelector('body');
-const input = document.querySelector('.myInput');
-const btn = document.querySelector('.btn');
+const input = document.querySelector(".myInput");
+const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", ()=>{
-    let userInput = input.value;
+btn.addEventListener("click", () => {
+  let userInput = input.value;
 
-    const getPrime = () => {
-        for(let i = 1; i < userInput; i++){
-            if(userInput < 1){
-                return(`${userInput} is not prime number`);
-            }
-            else if(i > 1 && userInput % i !== 0){
-                return (`${userInput} is a prime number`);
-            }else if(i > 1 && userInput % i === 0){
-                let factors = new Array();
-                factors.push(i);
-                return(`${userInput} is not a prime number, because ${factors} is a factor`);
-
-            }
-        }
+  const getPrime = () => {
+    for (let i = 1; i < userInput; i++) {
+      if (userInput < 1) {
+        return `${userInput} is not prime number`;
+      } else if (i > 1 && userInput % i !== 0) {
+        return `${userInput} is a prime number`;
+      } else if (i > 1 && userInput % i === 0) {
+        let factors = new Array();
+        factors.push(i);
+        return `${userInput} is not a prime number, because ${factors} is a factor`;
+      }
     }
+  };
 
-    console.trace(getPrime());
-})
+  console.trace(getPrime());
+});
 
 // btn.addEventListener('click', (e) => {
 //     console.log("clicked");
 // });
-
-
-
-
