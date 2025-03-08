17a1334b8f983588ff3b75bb9d295e3f1ac515e1
realkendo <kennethistifanus1@gmail.com>
2025-03-08 22:15:19 +0100

    network

--- YDKJS/Up&Going/chap1.js
+++ YDKJS/Up&Going/chap1.js
@@ -1,11 +1,6 @@
 console.log("loaded Successfully");
 
 function purchasePhone() {
-  // phone price
-  let phonePrice = Number(
-    prompt("What is the price of the phone you want to buy? ")
-  );
-
   // account balance
   let accountBalance = 100000;
   console.log("Starting balance- ", accountBalance);
@@ -14,10 +9,18 @@ function purchasePhone() {
   const SPENDING_THRESHOLD = 90000;
 
   // accessory price
-  const ACCESSORY_PRICE = 13000;
+  const ACCESSORY_PRICE = 10000;
 
   // TAX
   const TAX_RATE = 0.13;
+
+  let phonePrice;
+
+  // phone price
+  phonePrice = Number(
+    prompt("What is the price of the phone you want to buy? ")
+  );
+  accountBalance > SPENDING_THRESHOLD;
 
   // total cost of purchase
   let totalCost = phonePrice + ACCESSORY_PRICE + phonePrice * TAX_RATE;
